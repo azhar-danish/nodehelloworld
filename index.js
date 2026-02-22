e3b337d9efd06f919e626b57586cea52c0be9b0a
Azhar Danish <azhardanish@Azhars-MacBook-Air.local>
2026-02-22 15:03:21 +0530

    update the code

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ console.log('Hello World!');
 const number = [1, 2, 3, 4, 5];
 const sum = number.reduce((acc, curr) => acc + curr, 0);
 console.log('Sum:', sum);
+
+console.log('This is a new line of code added to the index.js file.');
