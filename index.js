4a52073370fc350d65cb2e7e8e492937d542d4a2
Azhar Danish <azhardanish@Azhars-MacBook-Air.local>
2026-02-26 12:19:24 +0530

    getting the average of the code

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ const sum = number.reduce((acc, curr) => acc + curr, 0);
 console.log('Sum:', sum);
 
 console.log('This is a new line of code added to the index.js file.');
+
+const num = [ 10, 20, 30, 40, 50];
+const average = num.reduce((acc, curr) => acc + curr, 0) / num.length;
+console.log('Average:', average);
